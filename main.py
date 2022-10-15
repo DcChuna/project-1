b1b16add207f0048731b45ad4cbab84a3767ad3b
Nazar <nazarhuzner@gmail.com>
2022-10-15 16:24:54 +0300

    +

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+n1 = int(input('number'))
+b1 = input('+ or - or * or /')
+n2 = int(input('number2'))
+
+if b1 == '+':
+    print(n1 + n2)
+if b1 == '-':
+    print(n1 - n2)
+if b1 == '/':
+    print(n1 / n2)
+if b1 == '*':
+    print(n1 * n2)
